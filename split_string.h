9f907bb38fec678c1379b13e4cb44aacc30ab80c
Evan Chime <evanchime@yahoo.co.uk>
2019-07-05 01:16:09 +0100

    Create split_string.h

---
+++ split_string.h
@@ -0,0 +1,19 @@
+/*
+    split strings on delimiter boundary
+*/
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
