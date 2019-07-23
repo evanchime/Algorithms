a5469a0441c8369521c39e031955b35e9d9505be
evan chime <evanchime@yahoo.co.uk>
2019-07-23 21:15:46 +0100

    First version of linked list

---
+++ linked_list.h
@@ -0,0 +1,44 @@
+/*
+	*linked list implementation
+*/	
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+        }
+
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
